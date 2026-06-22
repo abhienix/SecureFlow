d357f6ea2b66f3d30a9c5f4d678f8bcb3c7c40ba
abhienix <abhienix@gmail.com>
2026-06-23 00:18:24 +0530

    fix: restore claude_client.py and remove debug scripts

--- backend/list_tables.py
+++
@@ -1,8 +0,0 @@
-import psycopg2, os
-from dotenv import load_dotenv
-load_dotenv('.env')
-conn = psycopg2.connect(os.environ['DATABASE_URL'])
-cur = conn.cursor()
-cur.execute("SELECT tablename FROM pg_tables WHERE schemaname='public'")
-print(cur.fetchall())
-conn.close()
